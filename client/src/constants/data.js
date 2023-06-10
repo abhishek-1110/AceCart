d3044f9f9e3018383cbd114b52c5f2bc0e6dfa0d
Abhishek <abhishkbhi@gmail.com>
2023-06-10 17:33:19 +0530

    updated banner data as per ace cart

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -41,6 +41,10 @@ export const navData = [
 // for banners
 export const bannerData = [
   {
+    id: 7,
+    url: "https://github.com/abhishek-1110/AceCart/assets/57557012/010655cf-f8cb-4a81-b9b5-af95d63f8329"
+  },
+  {
     id: 0,
     url: "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/1a326d2f48aaedf0.jpg?q=50",
   },
@@ -49,29 +53,20 @@ export const bannerData = [
     url: "https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50",
   },
   {
-    id: 2,
-    url: "https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50",
-  },
-  {
-    id: 3,
-    url: "https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50",
-  },
-  {
-    id: 4,
-    url: "https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50",
-  },
-  {
-    id: 5,
-    url: "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/1d2a97eb4cc2cf4e.jpg?q=50",
-  },
-  {
     id: 6,
     url: "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/70559663485113a8.jpeg",
-  },
+  }, {
+    id: 8,
+    url: "https://github.com/abhishek-1110/AceCart/assets/57557012/69d8aa21-8375-455c-96ee-ae2a433cf347",
+  }, {
+    id: 9,
+    url: "https://github.com/abhishek-1110/AceCart/assets/57557012/d26c4cee-9098-48d5-b28f-03e4c9539491",
+  }  
 ];
 
 export const imageURL = [
   "https://rukminim1.flixcart.com/flap/960/960/image/2f30db9425df5cec.jpg?q=50",
-  "https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg",
+  // "https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg",
+  "https://github.com/abhishek-1110/AceCart/assets/57557012/801146ea-6b5e-4f31-a0ef-828c61a5e1f4",
   "https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50",
 ];
